14b76174038cf92db0cebeee0f0b67c4445c1ec3
TayWolf <alex.amanz.95@gmail.com>
2020-08-13 11:42:07 -0500

    BD del sistema

--- DB/task-app.sql
+++ DB/task-app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-08-2020 a las 17:39:02
+-- Tiempo de generación: 13-08-2020 a las 18:40:43
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.11
 
@@ -21,6 +21,50 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `task-app`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id_task` int(10) UNSIGNED NOT NULL,
+  `task_name` char(100) NOT NULL,
+  `task_description` char(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tasks`
+--
+
+INSERT INTO `tasks` (`id_task`, `task_name`, `task_description`) VALUES
+(1, 'Artículo', 'Debo escribir un articulo'),
+(2, 'Práctica de programación', 'Debo estudiar más POO y AJAX en PHP'),
+(3, 'Artículo para blog', 'Escribir un artículo PHP'),
+(4, 'Comprar ropa', 'Debo comprar ropa'),
+(6, 'Programar', 'Tengo que hacer un programa rápido'),
+(10, 'Crear Web', 'Debo crear un sitio web con PHP');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id_task`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id_task` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
